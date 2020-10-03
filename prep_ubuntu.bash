80d316f75bf69fd2d6661dab590621ea9a3bd65d
Kern3LP4niK <42155863+Kern3LP4niK@users.noreply.github.com>
2020-10-02 21:32:28 -0500

    Create prep_ubuntu.bash

---
+++ prep_ubuntu.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+update_with_apt()
+{
+  sudo apt update
+  sudo apt upgrade -y
+}
+apt_install_guest_additions()
+{
+  sudo apt install -y build-essential dkms linux-headers-$(uname -r)
+  sudo apt install -y virtualbox-guest-additions-iso
+}
+apt_install_ssh_server()
+{
+  sudo apt install -y openssh-server
+  sudo systemctl enable ssh
+  sudo systemctl start ssh
+}
+configure_grub()
+{
+  sudo echo 'GRUB_GFXMODE="1600x1200x32"'  >> /etc/default/grub
+  sudo echo 'GRUB_GFXPAYLOAD_LINUX="1600x1200x32"' >> /etc/default/grub
+  sudo update-grub
+}
+
+
