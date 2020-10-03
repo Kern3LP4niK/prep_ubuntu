fe1937ecd371396fbd73077d5840724d286ff986
Kern3LP4niK <42155863+Kern3LP4niK@users.noreply.github.com>
2020-10-03 09:49:28 -0500

    Update prep_ubuntu.bash

--- prep_ubuntu.bash
+++ prep_ubuntu.bash
@@ -20,10 +20,36 @@ configure_grub()
   sudo echo 'GRUB_GFXMODE="1600x1200x32"'  >> /etc/default/grub
   sudo echo 'GRUB_GFXPAYLOAD_LINUX="1600x1200x32"' >> /etc/default/grub
 }
+install_docker()
+{
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo apt-key fingerprint 0EBFCD88
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  sudo apt-get update
+}
+install_browsh()
+{
+  wget https://github.com/browsh-org/browsh/releases/download/v1.6.4/browsh_1.6.4_linux_amd64.deb
+  sudo apt install ./browsh_1.6.4_linux_amd64.deb
+  rm ./browsh_1.6.4_linux_amd64.deb
+  browsh
+}
 
 update_OS_with_apt
 apt_install_guest_additions
-apt_install_ssh_server
+
 configure_grub
 sudo apt install grub-customizer
+sudo grub-customerizer
 dpkg-reconfigure console-setup
+
+apt_install_ssh_server
+
+install_browsh
+#install_docker
+#docker pull browsh/browsh
+
